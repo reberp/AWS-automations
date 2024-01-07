e8c03dd37dc99c26e5ee8a59c4cad8b267f6e741
reberp <patrickreber@yahoo.com>
2024-01-06 18:20:57 -0600

    SG fix & standardize

--- tf/main.tf
+++ tf/main.tf
@@ -7,79 +7,67 @@ terraform {
   }
 }
 
-#locals {
-#  vpc-id = "${aws_vpc.test-vpc.id}"
-#}
-#output "vpc-id" {
-#  value = "${local.vpc-id}"
-#}
-
-variable "EC2_KEY" {
-    type        = string
-    description = "This is an example input variable using env variables."
-}
-
 provider "aws" {
   region  = var.instance["region"]
 }
 
 #create VPC
-resource "aws_vpc" "test-vpc" {
+resource "aws_vpc" "test_vpc" {
   cidr_block       = "10.10.0.0/16"
 
   tags = {
-    Name = "test-vpc"
+    Name = "test_vpc"
   }
 }
 
 #create subnet and put into vpc 
-resource "aws_subnet" "test-subnet" {
-  vpc_id     = aws_vpc.test-vpc.id
+resource "aws_subnet" "test_subnet" {
+  vpc_id     = aws_vpc.test_vpc.id
   cidr_block = "10.10.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "test-subnet"
+    Name = "test_subnet"
   }
 }
 
 #create IGW and attach to VPC
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
-resource "aws_internet_gateway" "test-gw" {
-  vpc_id = aws_vpc.test-vpc.id
+resource "aws_internet_gateway" "test_gw" {
+  vpc_id = aws_vpc.test_vpc.id
 
   tags = {
-    Name = "test-gw"
+    Name = "test_gw"
   }
 }
 
 #create routing table and to VPC to send to IGW\
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 resource "aws_route_table" "test_route_table" {
-  vpc_id = aws_vpc.test-vpc.id
+  vpc_id = aws_vpc.test_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.test-gw.id
+    gateway_id = aws_internet_gateway.test_gw.id
   }
 
   tags = {
-    Name = "test-rt"
+    Name = "test_rt"
   }
 }
 
 resource "aws_route_table_association" "test_route_table_association" {
-  subnet_id      = aws_subnet.test-subnet.id
+  subnet_id      = aws_subnet.test_subnet.id
   route_table_id = aws_route_table.test_route_table.id
 }
 
 
 #create security group
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.test-vpc.id
+resource "aws_security_group" "test_sg" {
+  name        = "test_sg"
+  description = "Allow all SSH and own SG"
+  vpc_id      = aws_vpc.test_vpc.id
 
   ingress {
     description      = "SSH"
@@ -88,7 +76,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  # from itself vpc [aws_vpc.main.cidr_block]
+  ingress {
+    description      = "own SG"
+    to_port          = 0
+    from_port        = 0
+    protocol         = "all"
+    self             = true
+  }
 
   egress {
     from_port        = 0
@@ -99,26 +93,25 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   tags = {
-    Name = "allow_ssh"
+    Name = "test_sg"
   }
 }
 
 resource "aws_instance" "app_server" {
   count                   = 2
-  ami                     = var.instance["ami-id"]
+  ami                     = var.instance["ami_id"]
   instance_type           = var.instance["type"]
   key_name                = var.ec2_keyname           
-  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
-  subnet_id               = aws_subnet.test-subnet.id
+  vpc_security_group_ids  = [aws_security_group.test_sg.id]
+  subnet_id               = aws_subnet.test_subnet.id
   tags = {
     Name = "${var.instance["name"]}.${count.index}"
   }
 }
-#[var.instance["vpc-security-group-ids"]] 
 
-#output "instance_ips" {
-#  value = [for e in aws_instance.app_server : e.public_ip]
-#}
+output "instance_ips" {
+  value = [for e in aws_instance.app_server : e.public_ip]
+}
 
 output "instance_names" {
   value = [for e in aws_instance.app_server : e.tags["Name"]]
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,10 +1,5 @@
-variable "instance-ami" {
-  type = string
-  default = "ami-0c7217cdde317cfec"
-}
-
 variable "ec2_keyname" {
-  description = "The type of the instance"
+  description = "Key to use for the instance. Overriden by Env"
   type        = string
   default     = "test"
 }
@@ -12,12 +7,9 @@ variable "ec2_keyname" {
 variable "instance" {
   type = map
   default = { #hopefully none of this stuff is intended to be hidden. Next step is to create new ones every time. 
-    "ami-id"                  = "ami-0c7217cdde317cfec"
+    "ami_id"                  = "ami-0c7217cdde317cfec" #ubuntu
     "type"                    = "t2.micro"
     "name"                    = "tf-test-create"
     "region"                  = "us-east-1"
-    "keyname"                 = ""
-    #"vpc-security-group-ids"  = ""
-    #"subnet-id"               = ""      
   }
 }
